8e12de6c2d67d2cfa41bccf5ed1d9cf18232b824
Mahdi Niyati <Niyat2612@gmail.com>
2023-06-16 11:53:37 +0330

    add timer

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let inputCounter = document.querySelector('#input-counter');
 let startCounter = document.querySelector('#start-counter');
 let errorElement = document.querySelector('#error-message');
 let timerCircle = document.querySelector('.c100');
+let timerNum = document.querySelector('.c100 > span');
+let loadingMessage = document.querySelector('.message > .loading');
+let successMessage = document.querySelector('.message > .success');
 
 
 startCounter.addEventListener("click", function(e){
@@ -17,4 +20,20 @@ startCounter.addEventListener("click", function(e){
     errorElement.classList.remove('active');
     startBox.style.display = 'none'
     timerCircle.style.display = 'block';
+    timerNum.innerText = seconds ;
+    loadingMessage.style.display = "block";
+    successMessage.style.display = "none";
+
+    let timerId = setInterval(() => {
+        if(seconds <= 1){
+            clearInterval(timerId);
+            startBox.style.display = "block"
+            timerCircle.style.display = "none"
+            inputCounter.value = '';
+            successMessage.style.display = "block";
+            loadingMessage.style.display = "none";
+        }
+        seconds -= 1 ;
+        timerNum.innerText = seconds ;
+    }, 1000);
 })
