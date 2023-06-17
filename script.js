3c1c0d8bc606750e3591a1e4387c2b8ad9ecfd62
Mahdi Niyati <Niyat2612@gmail.com>
2023-06-17 22:07:06 +0330

    timer

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ let successMessage = document.querySelector('.message > .success');
 
 startCounter.addEventListener("click", function(e){
     let seconds = +inputCounter.value;
-    console.log(seconds);
     if(seconds == 0){
         errorElement.innerHTML = 'زمان را به درستی وارد کنید';
         errorElement.classList.add('active');
@@ -24,16 +23,26 @@ startCounter.addEventListener("click", function(e){
     loadingMessage.style.display = "block";
     successMessage.style.display = "none";
 
+    let originalSeconds = seconds ;
+    let lastPercent = '';
     let timerId = setInterval(() => {
-        if(seconds <= 1){
+        if(lastPercent) timerCircle.classList.remove(lastPercent);
+        if(seconds <= 0){
             clearInterval(timerId);
             startBox.style.display = "block"
             timerCircle.style.display = "none"
             inputCounter.value = '';
             successMessage.style.display = "block";
             loadingMessage.style.display = "none";
+            return
         }
         seconds -= 1 ;
+        let percent = Math.floor(((originalSeconds - seconds) / originalSeconds * 100));
+        lastPercent = `p${percent}`
+        console.log(percent);
+        timerCircle.classList.add(`p${percent}`);
         timerNum.innerText = seconds ;
     }, 1000);
+    
+     
 })
